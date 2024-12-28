e092de0dbed4d97625c64ad41ee7d85dd89e6310
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-28 00:38:07 -0800

    Adjusted padding for nav links

--- star-wars-site/app/routes/layout.tsx
+++ star-wars-site/app/routes/layout.tsx
@@ -4,8 +4,8 @@ import { Link, Outlet } from 'react-router'
 export default function Layout() {
     return (
         <div className='flex flex-col min-h-svh'>
-            <nav className='flex justify-end p-4 border-b-2 border-black'>
-                <div className="black flex gap-x-10 pr-12">
+            <nav className='flex justify-center md:justify-end py-4 border-b-2 border-black'>
+                <div className="black flex gap-x-10 md:pr-16">
                     <Link to="/">Home</Link>
                     <Link to="/films">Films</Link>
                     <Link to="/people">People</Link>
